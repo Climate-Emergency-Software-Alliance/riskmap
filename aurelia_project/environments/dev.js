39f4fc91784b6d6f8cf2017129bd7f8864a4dfac
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-09-24 21:05:15 +0200

    updated enivronments

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -8,13 +8,17 @@ export default {
     title: 'RiskMap.us',
     report_timeperiod: 43200,
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data-dev.riskmap.us/',
     app: 'https://broward-dev.riskmap.us/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/1747847428843379'}, //FB page id is used as no username is allowd at this point.
+      { name: 'facebook', link: 'http://m.me/1747847428843379' }, //FB page id is used as no username is allowd at this point.
       //TODO after the twitter dev bot is registered
-      {name: 'twitter', link: 'https://twitter.com/messages/compose?recipient_id=905602080252977152&welcome_message_id=905919155492331523&text=/flood'}/*,
+      {
+        name: 'twitter',
+        link: 'https://twitter.com/messages/compose?recipient_id=905602080252977152&welcome_message_id=905919155492331523&text=/flood'
+      } /*,
       //Disable telegram button for US deployment
       {name: 'telegram', link: 'https://telegram.me/CognicityUS_bot'}*/
     ]
@@ -24,7 +28,8 @@ export default {
     report_timeperiod: 43200,
     default_language: 'id',
     environment: 'development',
-    tile_layer: 'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
     // data_server: 'http://localhost:8001/',
     data_server: 'https://dev-api.petabencana.id/',
     data_server_key: '{data_server_key}',
@@ -32,25 +37,26 @@ export default {
     // cards_server: 'http://localhost:4200/',
     app: 'https://dev.petabencana.id/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/CognicityDev.id'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana'}, //TODO : fill after twitter DM bot is created for Petabencana.
-      {name: 'telegram', link: 'https://telegram.me/CognicityDevBot'},
-      {name: 'telegram', link: 'https://telegram.me/CognicityDevBot'}
+      { name: 'facebook', link: 'http://m.me/CognicityDev.id' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana' }, //TODO : fill after twitter DM bot is created for Petabencana.
+      { name: 'telegram', link: 'https://telegram.me/CognicityDevBot' },
+      { name: 'telegram', link: 'https://telegram.me/CognicityDevBot' }
     ]
   },
   mapakalamidad: {
     title: 'Mapakalamidad.ph',
     report_timeperiod: 43200,
     default_language: 'tl',
-    tile_layer: 'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
     data_server: 'http://localhost:8001/',
     cards_server: 'http://localhost:4200/',
     app: 'https://dev.petabencana.id/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/CognicityDev.id'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana'}, //TODO : fill after twitter DM bot is created for Petabencana.
-      {name: 'telegram', link: 'https://telegram.me/CognicityDevBot'},
-      {name: 'telegram', link: 'https://telegram.me/CognicityDevBot'}
+      { name: 'facebook', link: 'http://m.me/CognicityDev.id' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana' }, //TODO : fill after twitter DM bot is created for Petabencana.
+      { name: 'telegram', link: 'https://telegram.me/CognicityDevBot' },
+      { name: 'telegram', link: 'https://telegram.me/CognicityDevBot' }
     ],
     data_server_key: '{data_server_key}'
   },
@@ -58,23 +64,25 @@ export default {
     title: 'RiskMap.in',
     report_timeperiod: 129600,
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data-dev.riskmap.in/',
     app: 'https://dev.riskmap.in/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/CognicityDevIndia'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Report+flood&via=CognicityDev_IN'},
-      {name: 'telegram', link: 'https://telegram.me/CognicityIN_bot'}
+      { name: 'facebook', link: 'http://m.me/CognicityDevIndia' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Report+flood&via=CognicityDev_IN' },
+      { name: 'telegram', link: 'https://telegram.me/CognicityIN_bot' }
     ]
   },
-  tabahinaqsha: {
-    title: 'TabahiNaqsha.pk',
+  aafatinfo: {
+    title: 'AafatInfo.pk',
     report_timeperiod: 129600,
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
-    data_server: 'https://dev-api.tabahinaqsha.pk/',
-    cards_server: 'https://cards-dev.tabahinaqsha.pk/',
-    app: 'https://dev.tabahinaqsha.pk/',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    data_server: 'https://dev-api.aafatinfo.pk/',
+    cards_server: 'https://cards-dev.aafatinfo.pk/',
+    app: 'https://dev.aafatinfo.pk/',
     deep_links: []
   }
 };
--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -7,13 +7,17 @@ export default {
     title: 'RiskMap.us',
     report_timeperiod: 3600,
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data.riskmap.us/',
     app: 'https://broward.riskmap.us/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/riskmapUS'},
+      { name: 'facebook', link: 'http://m.me/riskmapUS' },
       //TODO after the twitter dev bot is registered
-      {name: 'twitter', link: 'https://twitter.com/messages/compose?recipient_id=905602080252977152&welcome_message_id=905919155492331523&text=/flood'}/*,
+      {
+        name: 'twitter',
+        link: 'https://twitter.com/messages/compose?recipient_id=905602080252977152&welcome_message_id=905919155492331523&text=/flood'
+      } /*,
       //Disable telegram button for US deployment
       {name: 'telegram', link: 'https://telegram.me/riskmapus_bot'}*/
     ]
@@ -23,53 +27,57 @@ export default {
     report_timeperiod: 3600,
     default_language: 'id',
     environment: 'production',
-    tile_layer: 'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
     data_server: 'https://api.petabencana.id/',
     data_server_key: '{data_server_key}',
     cards_server: 'https://cards.petabencana.id/',
     app: 'https://petabencana.id/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/petabencana.id'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana'},
-      {name: 'telegram', link: 'https://telegram.me/BencanaBot'}
+      { name: 'facebook', link: 'http://m.me/petabencana.id' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana' },
+      { name: 'telegram', link: 'https://telegram.me/BencanaBot' }
     ]
   },
   mapakalamidad: {
     title: 'Mapakalamidad.ph',
     report_timeperiod: 3600,
     default_language: 'tl',
-    tile_layer: 'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/petabencana/ckq0nc6hp01vw17p9n17yxue2/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGV0YWJlbmNhbmEiLCJhIjoiY2s2MjF1cnZmMDlxdzNscWc5MGVoMTRkeCJ9.PGcoQqU6lBrcLfBmvTrWrQ',
     data_server: 'https://data.mapakalamidad.ph/',
     data_server_key: '{data_server_key}',
     cards_server: 'https://cards.mapakalamidad.ph/',
     app: 'https://mapakalamidad.ph/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/petabencana.id'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana'},
-      {name: 'telegram', link: 'https://telegram.me/BencanaBot'}
+      { name: 'facebook', link: 'http://m.me/petabencana.id' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Laporkan+banjir&via=petabencana' },
+      { name: 'telegram', link: 'https://telegram.me/BencanaBot' }
     ]
   },
   riskmap_in: {
     title: 'RiskMap.in',
     report_timeperiod: 43200, //12 hrs
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
     data_server: 'https://data.riskmap.in/',
     app: 'https://riskmap.in/',
     deep_links: [
-      {name: 'facebook', link: 'http://m.me/riskmapbot'},
-      {name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Report+flood&via=riskmapindia'},
-      {name: 'telegram', link: 'https://telegram.me/riskmapbot'}
+      { name: 'facebook', link: 'http://m.me/riskmapbot' },
+      { name: 'twitter', link: 'https://twitter.com/intent/tweet?text=Report+flood&via=riskmapindia' },
+      { name: 'telegram', link: 'https://telegram.me/riskmapbot' }
     ]
   },
-  tabahinaqsha: {
-    title: 'TabahiNaqsha.pk',
+  aafatinfo: {
+    title: 'AafatInfo.pk',
     report_timeperiod: 129600,
     default_language: 'en',
-    tile_layer: 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
-    data_server: 'https://api.tabahinaqsha.pk/',
-    cards_server: 'https://cards.tabahinaqsha.pk/',
-    app: 'https://tabahinaqsha.pk/',
+    tile_layer:
+      'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidXJiYW5yaXNrbWFwIiwiYSI6ImNqMnFraWVzYzAyd24ycXRqMmpvbmhyZ2QifQ.xc_v7umok760t2q6NZK1RA',
+    data_server: 'https://api.aafatinfo.pk/',
+    cards_server: 'https://cards.aafatinfo.pk/',
+    app: 'https://www.aafatinfo.pk/',
     deep_links: []
   }
 };
